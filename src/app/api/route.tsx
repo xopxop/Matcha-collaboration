02ed994ab78596317ec86aea8e538583fcd18740
du than <du.than@relexsolutions.com>
2024-10-22 15:57:21 +0300

    renovate(MATCHA-5): migrate page to app router

---
+++ src/app/api/route.tsx
@@ -0,0 +1,13 @@
+import exp from "constants"
+
+export async function GET() {
+  return new Response(JSON.stringify({ test: "get" }), {
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
+
+export async function POST() {
+  return new Response(JSON.stringify({ test: "post" }), {
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
