482951a24fe682f1a0521d201e55002c7b53d82e
esalorin <eena.salorinta@gmail.com>
2024-11-04 16:07:56 +0200

    MATCHA-17: Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,53 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
+    <div className="w-screen h-screen text-white text-center bg-gradient-to-tr from-[#EA4080] to-[#EE805F] grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pt-48 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <div className="flex items-center">
+          <Image
+          className=""
+          src="/icons/matcha-white.png"
           alt="Next.js logo"
-          width={180}
+          width={80}
           height={38}
           priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          />
+          <h1 className="text-5xl font-bold">matcha</h1>
+          </div>
+          <p className="text-xs font-light">By tapping Create Account or Sign In, you agree to our Terms. Learn how we process your data in our Privacy Policy and Cookies Policy.</p>
+        <ol className="list-inside list-none text-sm text-center">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            <Link href={'#'}>
+            <div className="w-80 h-12 rounded-full border-solid border-2 border-white flex items-center justify-center">
+              <p>
+                SIGN IN WITH GOOGLE
+              </p>
+            </div>
+            </Link>
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="mb-2">
+            <Link href={'#'}>
+            <div className="w-80 h-12 rounded-full border-solid border-2 border-white flex items-center justify-center">
+              <p>
+                SIGN IN WITH FACEBOOK
+              </p>
+            </div>
+            </Link>
+          </li>
+          <li className="mb-2">
+          <Link href={'/login'}>
+            <div className="w-80 h-12 rounded-full border-solid border-2 border-white flex items-center justify-center">
+              <p>
+                SIGN IN WITH EMAIL
+              </p>
+            </div>
+            </Link>
+          </li>
         </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <Link href="#" >Trouble Signing In?</Link>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
