f69dd40a3e9d290028160e770dba9b1959473f0c
Du Than <du.than@relexsolutions.com>
2025-01-03 15:31:11 +0200

    search box and profile components

--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -1,12 +1,30 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { clsx } from 'clsx';
 
 const SearchBar = () => {
   return (
-    <div className="relative h-12 w-full border border-gray-300 rounded-lg">
-      <MagnifyingGlassIcon className="absolute ml-5 h-12 w-5" />
-      <input placeholder="Search" />
+    <div className="h-12 w-full border border-gray-300 rounded-lg flex flex-row items-center gap-[11px]">
+      <MagnifyingGlassIcon className="ml-5 h-5 w-5" />
+      <input className="flex-1" placeholder="Search" />
       {/* <input type="text" placeholder="Search" className="w-full h-12" style={{border: '1px solid #E8E6EA', borderRadius: 15}} /> */}
       {/* <input type="text" placeholder="Search" className="w-full h-12" style={{border: '1px solid #E8E6EA', borderRadius: 15}} /> */}
+    </div>
+  );
+}
+
+const Profile = ({ highLighted = false }: { highLighted?: boolean }) => {
+  return (
+    <div
+      className={
+        clsx(
+          "flex-shrink-0 flex-grow-0 basis-auto h-[66px] w-[66px] rounded-full border flex items-center justify-center",
+          highLighted && "bg-gradient-to-tr from-[#f09433] via-[#dc2743] to-[#bc1888]"
+        )
+      } 
+    >
+      <div className="h-[58px] w-[58px] rounded-full border">
+        <img src="/images/pug.jpeg" alt="Profile" className="w-full h-full rounded-full object-cover" />
+      </div>
     </div>
   );
 }
@@ -21,7 +39,22 @@ export default function Page() {
       <SearchBar />
       <div>
         <h1>Activities</h1>
-        <div style={{height: 91, backgroundColor: 'red'}}>User</div>
+        <div
+          className="flex flex-row gap-[15px] overflow-x-scroll"
+        >
+          <Profile highLighted={true} />
+          <Profile />
+          <Profile highLighted={true} />
+          <Profile />
+          <Profile />
+          <Profile highLighted={true} />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile highLighted={true} />
+          <Profile />
+          <Profile />
+        </div>
       </div>
       <div>
         <h1>Messages</h1>
