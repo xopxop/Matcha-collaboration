368d79f273c61a5af3dcd1ffd46b634ec6e59cb7
Du Than <du.than@relexsolutions.com>
2025-01-03 09:55:27 +0200

    feature(Matcha-19): Added template for message page

--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -1,7 +1,31 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
+const SearchBar = () => {
+  return (
+    <div className="relative h-12 w-full border border-gray-300 rounded-lg">
+      <MagnifyingGlassIcon className="absolute ml-5 h-12 w-5" />
+      <input placeholder="Search" />
+      {/* <input type="text" placeholder="Search" className="w-full h-12" style={{border: '1px solid #E8E6EA', borderRadius: 15}} /> */}
+      {/* <input type="text" placeholder="Search" className="w-full h-12" style={{border: '1px solid #E8E6EA', borderRadius: 15}} /> */}
+    </div>
+  );
+}
+
 export default function Page() {
   return (
-    <div>
-      <h1>This is messages page</h1>
+    <div style={{ paddingLeft: 40, paddingRight: 40 }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <h1>Message</h1>
+        <button>Filter Button</button>
+      </div>
+      <SearchBar />
+      <div>
+        <h1>Activities</h1>
+        <div style={{height: 91, backgroundColor: 'red'}}>User</div>
+      </div>
+      <div>
+        <h1>Messages</h1>
+      </div>
     </div>
   );
 }
